10d6ec4652c0f3b270c15f1c113579085d50bdf2
xlisp <1918530798@qq.com>
2018-10-14 15:39:08 +0800

    923.三数之和的多种可能

--- contest/WeeklyContest_106/923.cpp
+++ contest/WeeklyContest_106/923.cpp
@@ -2,6 +2,9 @@
 // Created by eden on 18-10-14.
 //
 
+// 923.三数之和的多种可能
+// https://leetcode-cn.com/problems/3sum-with-multiplicity/
+
 #include <vector>
 #include <algorithm>
 #include <iostream>
@@ -10,24 +13,27 @@ using namespace std;
 
 class Solution {
 public:
-    int threeSumMulti(vector<int>& A, int target) {
+    int dp[3][305];
 
-        int size = A.size();
-        long long res = 0;
-        for(int i = 0;i<size;++i) {
+    int threeSumMulti(vector<int>& A, int target)
+    {
 
-            for (int j = i+1; j < size; ++j) {
+        const int mod = 1e9 + 7;
+        int len = (int)A.size();
+        memset(dp, 0, sizeof(dp));
+        for(int i = 0; i < len; i++) {
+            for (int j = 2; j >= 1; j--)  // j==1 表示用2个数拼 target
+                for (int k = target; k >= A[i]; k--)
 
-                for (int k = j+1; k < size; ++k) {
-
-                    if (A[i] + A[j] + A[k] == target) {
-                        res = (++res) % (1000000000 + 7);
-                    }
+                {           // dp[j][k] 表示用 j-1 个数来拼出 target
+                    dp[j][k] = (dp[j][k] + dp[j - 1][k - A[i]]) % mod;  // 所以可以得到这个状态转移方程
+                    cout << "j:" << j << "k: " << k << "$$$" << dp[j][k] <<  endl;
                 }
-            }
+
+            dp[0][A[i]] = (dp[0][A[i]] + 1) % mod;   // 实际上就是j==0的情况 即只用一个数来组成target
+            cout << A[i] << "&&" << endl;
 
         }
-        return res;
-
+        return dp[2][target];
     }
 };
