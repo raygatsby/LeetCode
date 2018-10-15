aa7fcd1d0c15c5f568f1c9b0848806a476d5e08e
xlisp <1918530798@qq.com>
2018-10-15 12:53:54 +0800

    5.最长回文子串（加上注释）

--- 5.LongestPalindromicSubstring/5.cpp
+++ 5.LongestPalindromicSubstring/5.cpp
@@ -5,51 +5,54 @@
 #include <string>
 using namespace std;
 
+using namespace std;
 class Solution {
 public:
     string longestPalindrome(string s) {
-
-        if(s.length()==0){
-            return "";
+        int size = s.size();
+        if(size<=1){
+            return s;
         }
-
-        int dp[s.length()][s.length()] = {0};
-
-        for(int i = 0;i<s.length();i++){
+        // dp[i][j] 表示从下标 i到j 的最小回文子串
+        int dp[size][size];
+        memset(dp,0, sizeof(int)*size*size);
+        // 对角线置为1
+        for(int i = 0;i<size;++i){
             dp[i][i] = 1;
         }
+        int res_start;
+        int res_len;
+        int Max = 0;
+        for (int l = 2; l <= size; ++l) {
+            for(int i =0;i<size-l+1;++i ){
+                int j = i+l-1;
+                // i,j可以覆盖矩形的右上部分
 
-        int m = 0,n = 0;
-        int Max = 1;
-
-        for(int j = 1;j<s.length();j++){
-
-
-            for(int i = 0;i<=s.length()-2;i++){
-
-                if(i+j<s.length()){
-                    if(s[i]==s[i+j]){
-
-                        if(j>=2&&dp[i+1][i+j-1]!=j-1){
-                            dp[i][i+j] = max(dp[i][i+j-1],dp[i+1][i+j]);
-                        }else{
-                            dp[i][i+j] = dp[i+1][i+j-1] + 2;
+                if(s[i]==s[j]){
+                    if(i+1==j){
+                        // 当 i，j正好挨着
+                        dp[i][j] = 2;
+                    } else{
+                        if(dp[i+1][j-1]==j-i-1){
+                            // i+1 ---> j-1是一个回文串
+                            dp[i][j] = dp[i+1][j-1]+2;
+                        } else{
+                            // 否则 看去掉 i，j中的哪一个字符
+                            dp[i][j] = max(dp[i+1][j],dp[i][j-1]);
                         }
-
-
-                    }else{
-                        dp[i][i+j] = max(dp[i][i+j-1],dp[i+1][i+j]);
                     }
+                } else{
+                    // i,不等于j直接取两种情况的最大值
+                    dp[i][j] = max(dp[i+1][j],dp[i][j-1]);
+                }
 
-                    if(dp[i][i+j]>Max) {
-                        Max = dp[i][i+j];
-                        m = i;
-                        n = i+j;
-                    }
+                if(dp[i][j] > Max){
+                    Max = dp[i][j];
+                    res_start = i;
                 }
             }
         }
-        return s.substr(m,Max);
+        return s.substr(res_start,Max);
     }
 };
 
