14bf21cb283898d45b300af5d3568476daf890dc
xlisp <1918530798@qq.com>
2018-10-24 09:54:37 +0800

    740.删除与获得点数

--- 740.DeleteandEarn/740.cpp
+++ 740.DeleteandEarn/740.cpp
@@ -26,9 +26,92 @@ public:
             // 取最大值
             dp[i] = max(dp[i-1],dp[i-2]+sum[i]);
         }
-
         return dp[10000];
-
-
     }
 };
+
+//11.34
+// 开始写的超时
+//删除与获得点数
+#if 0
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+
+        int size = nums.size();
+
+        if(size==0){
+            return 0;
+        }
+
+        sort(nums.begin(),nums.end());
+
+        long long dp[size][size];
+
+        for(int i = 0;i<size;++i){
+            for(int j = 0;j<size;++j){
+                dp[i][j] = 0;
+            }
+            dp[i][i] = nums[i];
+        }
+
+
+
+        long long res = 0;
+
+        for(int l = 2;l<=size;++l){
+            for(int i = 0;i<size-l+1;++i){
+                int j = i+l-1;
+                long long Max = LONG_LONG_MIN;
+
+                for(int k = i;k<=j;++k){
+                    if(k!=i && nums[k]==nums[k-1]){
+                        Max = dp[i][j];
+                        break;
+                    }
+
+
+                    long long tmp = 0;
+                    int left,right;
+                    for(left=k;left>=i;--left){
+                        if(nums[left]==nums[k]){
+                            tmp+=nums[left];
+                            if(left==i){
+                                break;
+                            }
+
+                        }else if(nums[left]!=nums[k]-1 || left==i){
+                            break;
+                        }
+                    }
+
+                    if(nums[left]!=nums[k] && nums[left]!=nums[k]-1){
+                        tmp += dp[i][left];
+                    }
+
+                    for(right=k;right<=j;++right){
+
+                        if(nums[right]==nums[k]){
+                            tmp+=nums[right];
+                            if(right==j){
+                                break;
+                            }
+                        } else if(nums[right]!=nums[k]+1 || right==j){
+                            break;
+                        }
+                    }
+
+                    if(nums[right]!=nums[k] && nums[right]!=nums[k]+1){
+                        tmp+=dp[right][j];
+                    }
+
+                    tmp -= nums[k];
+                    Max = max(Max,tmp);
+                }
+                dp[i][j] = Max;
+            }
+        }
+        return dp[0][size-1];
+    }
+};
+#endif
